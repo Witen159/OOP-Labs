14af876299d3027e3c0a597a61819a46fcf345e3
Witen159 <44116296+Witen159@users.noreply.github.com>
2021-09-26 21:05:43 +0300

    Add: implementation of some functions

--- Shops/Shop.cs
+++ Shops/Shop.cs
@@ -3,21 +3,23 @@
     public class Shop
     {
         private static int _currentShopId = 1;
-        public Shop(string shopName)
+        public Shop(string shopName, string adress)
         {
             ShopName = shopName;
+            Adress = adress;
             ShopId = _currentShopId;
             _currentShopId++;
         }
 
         public string ShopName { get; }
+        public string Adress { get; }
         public int ShopId { get; }
 
         public void AddProducts()
         {
         }
 
-        public void Buy(Person customer)
+        public void Buy(Person customer, Product product, int numberOfProducts)
         {
         }
     }
--- Shops/ShopManager.cs
+++ Shops/ShopManager.cs
@@ -4,17 +4,29 @@ namespace Shops
 {
     public class ShopManager
     {
+        private List<Product> _availableProducts = new List<Product>();
         private List<Shop> _shops = new List<Shop>();
         public ShopManager()
         {
         }
 
-        public Shop AddShop(string shopName)
+        public Shop AddShop(string shopName, string shopAdress)
         {
+            var newShop = new Shop(shopName, shopAdress);
+            _shops.Add(newShop);
+            return newShop;
         }
 
         public Product RegisterProduct(string productName)
         {
+            Product newProduct = new Product(productName);
+            _availableProducts.Add(newProduct);
+            return newProduct;
+        }
+
+        public Shop FindTheBestOffer(Product product, int numberOfProducts)
+        {
+            return;
         }
     }
 }
