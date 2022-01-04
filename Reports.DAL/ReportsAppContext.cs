58bcd780d3c796fc50aa3ef8dba92f3f3a98cd16
Witen159 <44116296+Witen159@users.noreply.github.com>
2022-01-04 20:14:31 +0300

    feat: ReportAppContext, EntityFramework

---
+++ Reports.DAL/ReportsAppContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Reports.DAL.Entities;
+
+namespace Reports.DAL
+{
+    public class ReportsAppContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<Report> Reports { get; set; }
+
+        public ReportsAppContext()
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
