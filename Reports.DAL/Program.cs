a4dcb4baf37f64d1d4fe200ed038889183ae6a23
Witen159 <44116296+Witen159@users.noreply.github.com>
2022-01-04 19:52:06 +0300

    feat: Reports Client and DAL projects

---
+++ Reports.DAL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reports.DAL
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
