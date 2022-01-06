45ee0787217356e7ed46924aca49c3d81ed6378a
Witen159 <44116296+Witen159@users.noreply.github.com>
2022-01-06 18:20:40 +0300

    feat: EmployeeController

---
+++ Reports.Server/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Reports.DAL.Entities;
+using Reports.Server.Services;
+
+namespace Reports.Server.Controllers
+{
+    [ApiController]
+    [Route("/employees")]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _service;
+
+        public EmployeeController(IEmployeeService service)
+        {
+            _service = service;
+        }
+        
+        [HttpPost]
+        public Employee Create([FromQuery] string name)
+        {
+            return _service.Create(name);
+        }
+    }
+}
--- Reports.Server/Startup.cs
+++ Reports.Server/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Reports.Server.Services;
 
 namespace Reports.Server
 {
@@ -32,6 +33,7 @@ namespace Reports.Server
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Reports.Server", Version = "v1" });
             });
+            services.AddScoped<IEmployeeService, EmployeeService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
