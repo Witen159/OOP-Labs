3e2c6dc1705a8f6074929834b1652a560f8e851a
Witen159 <44116296+Witen159@users.noreply.github.com>
2022-01-17 19:42:41 +0300

    feat: Reports.Client Console application

---
+++ Reports.Client/Application.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Reports.Client.Managers;
+using Reports.Client.Managers.Employee_manager;
+using Reports.Client.Managers.Report_manager;
+using Reports.Client.Managers.Task_manager;
+
+namespace Reports.Client
+{
+    public class Application
+    {
+        private IManager _employeeManager = new EmployeeManager();
+        private IManager _taskManager = new TaskManager();
+        private IManager _reportManager = new ReportManager();
+        public Application()
+        {
+            Console.WriteLine("Please use command numbers is our application");
+            Console.WriteLine();
+        }
+        public void StartApplication()
+        {
+            Console.WriteLine("Choose manager:");
+            Console.WriteLine("1. Employee manager");
+            Console.WriteLine("2. Task manager");
+            Console.WriteLine("3. Report manager");
+            Console.WriteLine("4. Exit Menu");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case 1:
+                    _employeeManager.Manager();
+                    break;
+                case 2:
+                    _taskManager.Manager();
+                    break;
+                case 3:
+                    _reportManager.Manager();
+                    break;
+                case 4:
+                    return;
+                default:
+                    Console.WriteLine("Wrong command");
+                    break;
+            }
+
+            StartApplication();
+        }
+    }
+}
--- Reports.Client/Program.cs
+++ Reports.Client/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main( string[] args )
         {
+            Application application = new Application();
+            application.StartApplication();
         }
     }
 }
